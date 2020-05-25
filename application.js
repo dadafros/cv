55621bbd1ddc4316f6c7ec5a00a7911d82b8597f
Davi Frossard <davi_bf@outlook.com>
2020-05-25 14:32:23 -0400

    homepage

---
+++ application.js
@@ -0,0 +1,26 @@
+function download(){
+    name = document.querySelector('#name').value
+    company = document.querySelector('#company').value
+    email = document.querySelector('#email').value
+    sendEmail(name, company, email);
+
+	var element = document.createElement('a');
+	element.setAttribute('href', encodeURIComponent("resume.pdf"));
+	element.setAttribute('download', "ResumeDaviFrossard");
+	document.body.appendChild(element);
+	element.click();
+	document.body.removeChild(element);
+}
+
+function sendEmail(name, company, email) {
+	Email.send({
+	Host: "smtp.gmail.com",
+	Username : "use a gmail to send email",
+	Password : "passwd for gmail",
+	To : 'sendto',
+	From : "gmail",
+	Subject : "Dowloaded Resume from your site!",
+	Body : "Hello Mr. Frossard, we are glad to tell you that your resume was downloaded from (name, company, email) " + name + ", " + company + ", " + email,
+	}).then(
+	);
+}
